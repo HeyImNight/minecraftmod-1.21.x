1d2db42491c005c5129c74d5482c4d2c803fddf5
Night <nightismehello@gmail.com>
2025-07-20 18:15:43 -0400

    Initial commit

---
+++ src/main/java/net/jrake/minecraftmod/minecraftModClient.java
@@ -0,0 +1,17 @@
+package net.jrake.minecraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class minecraftModClient implements ClientModInitializer
+{
+
+
+
+
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
