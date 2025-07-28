d81c701040f6268df7d3c9a2cb4ecc387b004246
JJ <nightismehello@gmail.com>
2025-07-28 15:04:32 -0400

    FLOWER DONE BOTCH YAYAYAYAYA

--- src/main/java/net/jrake/minecraftmod/block/ModBlocks.java
+++ src/main/java/net/jrake/minecraftmod/block/ModBlocks.java
@@ -1,12 +1,15 @@
 package net.jrake.minecraftmod.block;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.mixin.content.registry.AbstractFurnaceBlockEntityMixin;
 import net.jrake.minecraftmod.MinecraftMod;
 import net.jrake.minecraftmod.item.ModItems;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.FlowerBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.component.type.SuspiciousStewEffectsComponent;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -17,16 +20,10 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks
 {
-    //Creating the block BUT we can also copy from existing blocks
-    public static final Block FIRE_FLOWER_BLOCK = registerBlock("fire_flower_block", new Block(
-            AbstractBlock.Settings.create()
-                    .strength(5f)
-                    .ticksRandomly()
-                    .breakInstantly()
-                    .sounds(BlockSoundGroup.CHERRY_SAPLING)
-                    .pistonBehavior(PistonBehavior.DESTROY)
-                    .noCollision()
-                    .mapColor(MapColor.DARK_RED)));
+
+    //Flowers -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        //Creating the block BUT we can also copy from existing blocks
     public static final Block WATER_FLOWER_BLOCK = registerBlock("water_flower_block", new Block(
             AbstractBlock.Settings.create()
                     .strength(5f)
@@ -36,22 +33,31 @@ public class ModBlocks
                     .pistonBehavior(PistonBehavior.DESTROY)
                     .noCollision()
                     .mapColor(MapColor.DARK_RED)));
+    public static final FlowerBlock FIRE_FLOWER_BLOCK = registerFlowerBlock("fire_flower_block", new FlowerBlock(SuspiciousStewEffectsComponent.DEFAULT,
+            AbstractBlock.Settings.create()
+                    .strength(5f)
+                    .ticksRandomly()
+                    .breakInstantly()
+                    .sounds(BlockSoundGroup.CHERRY_SAPLING)
+                    .pistonBehavior(PistonBehavior.DESTROY)
+                    .noCollision()
+                    .mapColor(MapColor.DARK_RED)));
 
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+    //Helper Methods
+    private static FlowerBlock registerFlowerBlock(String name, FlowerBlock block)
+    {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK,Identifier.of(MinecraftMod.MOD_ID, name), block);
+    }
 
-
-
-
-
-
-    //Helper Method
     private static Block registerBlock(String name, Block block)
     {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK,Identifier.of(MinecraftMod.MOD_ID, name), block);
     }
 
-    //Helper Method
     //Since thi is a block, you need to initialize the corresponding item with it as well
     private static void registerBlockItem(String name, Block block)
     {
